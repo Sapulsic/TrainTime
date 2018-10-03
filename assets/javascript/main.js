4732c3a3ce26436f435827e30ec3eb0df4b7fab2
Sapulsic <carlos.barahona1995@gmail.com>
2018-10-03 01:05:00 -0500

    made some fixes and changes

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -2,7 +2,7 @@
 //  ============================================
 // Arrays and Variables for initiating Data
 
-var config = {
+  var config = {
     apiKey: "AIzaSyAkmFlUFGy0-4IGMPwJLrxj1WXRpKB7Wew",
     authDomain: "solrtraintime.firebaseapp.com",
     databaseURL: "https://solrtraintime.firebaseio.com",
@@ -16,55 +16,55 @@ var config = {
 
 // Functions
 //  ============================================
-
   $('#trainSubmit').on('click', function(event) {
-      event.preventDefault();
-
+    event.preventDefault();
+    
     var trainName = $('#trainName-input').val().trim();
     var trainDest = $('#trainDestination-input').val().trim();
-    var trainTime = moment($('#trainTime-input').val().trim(), "HH:MM").format('X');
+    var trainTime = moment(($('trainTime-input')).val(), "hh:mm a").format('HH');
     var trainFreq = $('#trainFreq-input').val().trim();
-
+    
     var  newTrain = {
-        name: trainName,
-        dest: trainDest,
-        time: trainTime,
-        freq: trainFreq
+      name: trainName,
+      dest: trainDest,
+      time: trainTime,
+      freq: trainFreq
     };
-
+    
     database.ref().push(newTrain);
-
+    
     $('#trainName-input').val("");
     $('#trainDestination-input').val("");
     $('#trainTime-input').val("");
     $('#trainFreq-input').val("");
   });
-
+  
   database.ref().on("child_added", function(trainSnapshot) {
-
+    
     var trainName = trainSnapshot.val().name;
     var trainDest = trainSnapshot.val().dest;
     var trainTime = trainSnapshot.val().time;
     var trainFreq = trainSnapshot.val().freq;
     
     // var trainRemainingTime = 
-
+    
     $("#formTrain > tbody").append(
       $("<td>").text(trainName),
       $("<td>").text(trainDest),
       $("<td>").text(trainTime),
-      $("<td>").text(trainFreq),
-      $("<td>").text()
+      $("<td>").text(trainArrival),
+      $("<td>").text(trainFreq)
     );
-
-
-  }, function (errorObject) {
-    console.log("Errors handled: " + errorObject.code);
-});
-
-            
+      
+      
+    }, function (errorObject) {
+      console.log("Errors handled: " + errorObject.code);
+    });
+    
+    
     // Testing / Debugging
-
-
-        // Main Process
+    
+    
+    // Main Process
     //  ============================================
+
